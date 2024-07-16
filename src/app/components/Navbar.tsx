ee26b35de48c19d844b9c0de5d810c552e19bb0a
dscmari <marian@noekel@gmail.com>
2024-07-16 18:21:58 +0200

    implement navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,13 +1,23 @@
 'use client'
 import React, { useEffect, useState } from 'react'
-import BurgerMenu from './BurgerMenu';
+import Link from 'next/link';
+
 
 export default function Navbar() {
 
     const [burgermenu, setBurgermenu] = useState(false)
+    const [showMenu, setShowMenu] = useState(false)
+
+    const toggleMenu = () => {
+        toggleX()
+        setShowMenu(!showMenu)
+    }
 
     const handleResize = () => {
         setBurgermenu(window.innerWidth < 860);
+        if(window.innerWidth > 860) {
+            setShowMenu(false)
+        }
     };
     
 
@@ -17,32 +27,62 @@ export default function Navbar() {
         handleResize();
       }, []);
 
+ 
+    const toggleX = () => {
+        const burgerMenu = document.getElementById("burger-menu");
+        const spans = burgerMenu?.getElementsByTagName('span');
+
+        if (spans) {
+            Array.from(spans).forEach((span) => {
+                span.classList.toggle('open');
+            });
+        }
+    }
 
     if(burgermenu) {
         return(
-            <div>
-                <BurgerMenu/>
+            <div className='p-4 border-b-4'>
+                <div className='flex justify-between items-center'>
+                    <img className='m-4' src="#" alt="namaste-websites logo" />
+                    <div className="burgermenu" id='burger-menu' onClick={toggleMenu}>
+                        <span className="top"></span>
+                        <span className="middle"></span>
+                        <span className="bottom"></span>
+                    </div>
+                </div>
+                <div className={showMenu ? 'block' : 'hidden'}>
+                    <ul className='flex flex-col items-end gap-4 mr-6 text-2xl py-4'>
+                        <li><Link href="#">Home</Link></li>
+                        <li><Link href="#">Leistungen</Link></li>
+                        <li><Link href="#">Referenzen</Link></li>
+                        <li><Link href="#">Über mich</Link></li>
+                        <li><Link href="#">Kontakt</Link></li>
+                    </ul>
+                </div>
             </div>
         )
     } else {
         return(
-            <div className='flex justify-around p-16 border-'>
+            <div className='border-b-4 flex justify-around p-12 items-center'>
                 <img src="#" alt="namaste-websites logo" />
                 <ul className='flex gap-4'>
                     <li className='p-4 hover:underline hover:cursor-pointer'>
-                        Leistungen
+                        Website-Erstellung
                     </li>
                     <li className='p-4 hover:underline hover:cursor-pointer'>
-                        Referenzen
+                        Preise
                     </li >
+                    <li className='p-4 hover:underline hover:cursor-pointer'>
+                        Referenzen
+                    </li>
+                    <li className='p-4 hover:underline hover:cursor-pointer'>
+                        Tipps
+                    </li>
                     <li className='p-4 hover:underline hover:cursor-pointer'>
                         Über mich
                     </li>
                     <li className='p-4 hover:underline hover:cursor-pointer'>
                         Kontakt
-                    </li>
-                    <li className='p-4 hover:underline hover:cursor-pointer'>
-                        Datenschutzerklärung
                     </li>
                 </ul>
             </div>
