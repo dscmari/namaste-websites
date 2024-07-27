df5d66e70807a84c7f0788c302840d7b7c3e8dab
Marian Noekel <marian@MacCarlo.fritz.box>
2024-07-27 16:18:52 +0200

    add content to /AGBs

--- src/app/AGBs/page.tsx
+++ src/app/AGBs/page.tsx
@@ -1,7 +1,179 @@
-import React from 'react'
+import React from "react";
 
 export default function AGBs() {
   return (
-    <div>AGBs geladen</div>
-  )
+    <div className="p-4 lg:mx-16">
+      <h1 className="lg:text-xl">Allgemeine Geschätsbedingungen</h1>
+      <h2 className="pb-12">für den Webdesigner Marian Nökel</h2>
+      <p className="border-b-2 mb-4 lg:mb-8">
+        Marian Nökel, Schlierseestraße 10, 81541 München, 146/110/71311
+      </p>
+      <h1>1. Geltungsbereich</h1>
+      <div className="ml-4 lg:ml-12">
+        <p>
+          1.1 &nbsp;&nbsp; Die nachstehenden allgemeinen Geschäftsbedingungen
+          gelten für alle Rechtsgeschäfte des Webdesigners Marian Nökel nach
+          diesem Vertrag mit seinem Vertragspartner, nachstehend "Kunde"
+          genannt.
+        </p>
+        <p>
+          1.2 &nbsp;&nbsp; Änderungen dieser Geschäftsbedingungen werden dem
+          Kunden schriftlich bekannt gegeben. Sie gelten als genehmigt, wenn der
+          Kunde nicht schriftlich Widerspruch erhebt. Der Kunde muss den
+          Widerspruch innerhalb von vier Wochen nach Bekanntgabe der Änderungen
+          an den Webdesigner absenden.
+        </p>
+      </div>
+      <h1 className="mt-4">2. Weitergeleitete Aufträge</h1>
+      <div className="ml-4 lg:ml-12">
+        <p>
+          2.1 &nbsp;&nbsp; Wenn ein Auftrag in der Form ausgeführt wird, dass
+          der Webdesigner einen Dritten mit der weiteren Erledigung betraut,
+          erfüllt der Webdesigner den Auftrag dadurch, dass er ihn im eigenen
+          Namen an den Dritten weiterleitet.
+        </p>
+        <p>
+          2.2 &nbsp;&nbsp; Die Angebote des Webdesigners sind frei bleibend und
+          unverbindlich. Gültig sind die im individuellen Angebot genannten
+          Preise; alle anderen Preise sind in der aktuellen Preisliste
+          aufgeführt.
+        </p>
+      </div>
+      <h1 className="mt-4">3. Zustandekommen des Vertrages</h1>
+      <div className="ml-4 lg:ml-12">
+        <p>
+          3.1 &nbsp;&nbsp; Ein Vertrag mit dem Webdesigner kommt durch die
+          Übermittlung des unterschriebenen Kundenauftrags per Fax, E-Mail oder
+          durch Zusendung der Auftragsbestätigung durch den Webdesigner
+          zustande.
+        </p>
+        <p>3.2 &nbsp;&nbsp; Der Gegenstand des Vertrages ist folgender:</p>
+        <div className="ml-4 lg:ml-8">
+          <p>
+            1. &nbsp;&nbsp; Leistungsumfang: Der Webdesigner erbringt
+            Dienstleistungen wie Webdesign, Webentwicklung, Wartung und Support.
+            Die genauen Leistungen werden im individuellen Vertrag festgelegt.
+          </p>
+          <p>
+            2. &nbsp;&nbsp; Zeitplan: Der Zeitplan für die Erbringung der
+            Dienstleistungen wird im jeweiligen Vertrag vereinbart.
+          </p>
+          <p>
+            3. &nbsp;&nbsp; Vergütung: Die Vergütung und Zahlungsmodalitäten
+            werden im individuellen Vertrag festgelegt.
+          </p>
+        </div>
+      </div>
+      <h1 className="mt-4">4. Daten</h1>
+      <p className="ml-4 lg:ml-12">
+        Der Kunde stellt den Webdesigner von sämtlichen Ansprüchen Dritter
+        hinsichtlich der überlassenen Daten frei. Im Falle eines Datenverlustes
+        kann der Webdesigner nicht haftbar gemacht werden, sofern dieser nicht
+        vorsätzlich oder grob fahrlässig handelt. Der Kunde verpflichtet sich,
+        bei Nichtverschulden bzw. Vertretenmüssen des Webdesigners, alle
+        erforderlichen Daten erneut unentgeltlich an den diesen zu übermitteln.
+      </p>
+      <h1 className="mt-4">5. Datenschutz</h1>
+      <p className="ml-4 lg:ml-12">
+        Der Vertragspartner erklärt sich damit einverstanden, dass im Rahmen des
+        mit ihm abgeschlossenen Vertrages, Daten über seine Person gespeichert,
+        geändert und oder gelöscht und im Rahmen der Notwendigkeit an Dritte
+        übermittelt werden. Dies gilt insbesondere für die Übermittlung von
+        Daten, die für die Anmeldung und oder Änderung einer Domain
+        (Internetadresse) notwendig sind.
+      </p>
+      <h1 className="mt-4">6. Preise und Zahlungen</h1>
+      <p>
+        Die Rechnungsstellung erfolgt im Voraus nach der gültigen Preisliste des
+        Webdesigners bzw. des individuell mit dem Kunden vereinbarten Preises.
+        Alle Rechnungen sind sofort und ohne Abzug zahlbar. Nach Eingang des
+        Rechnungsbetrages beim Webdesigner und Vorliegen der für die Leistungen
+        notwendigen Daten des Kunden, werden die vertraglich vereinbarten
+        Leistungen vom Webdesigner ausgeführt. Ist der Kunde mit fälligen
+        Zahlungen im Verzug, behält sich der Webdesigner vor, weitere Leistungen
+        bis zum Ausgleich des offenen Betrages nicht auszuführen und hieraus
+        entstandene Kosten an den Kunden weiterzugeben. Einwendungen wegen
+        Unrichtigkeit oder Unvollständigkeit eines Rechnungsabschlusses hat der
+        Kunde spätestens innerhalb von vier Wochen nach dessen Zugang zu
+        erheben; macht er seine Einwendungen schriftlich geltend, genügt die
+        Absendung innerhalb der -Wochen-Frist. Das Unterlassen rechtzeitiger
+        Einwendungen gilt als Genehmigung.
+      </p>
+      <h1 className="mt-4">7. Markenrechte/Copyrights</h1>
+      <p>
+        Der Kunde ist verpflichtet, alle rechtliche Verantwortung zu übernehmen,
+        im Hinblick auf Urheberschutz, Jugendschutz, Presserecht und das "Recht
+        am eigenen Bild". Für vom Kunden beauftragte Veröffentlichungen sind nur
+        Texte und Bilder zu veröffentlichen bzw. zur Veröffentlichung zur
+        Verfügung zu stellen, an denen ein entsprechendes Nutzungsrecht besteht
+        und zu denen das ggf. erforderliche Einverständnis abgebildeter Personen
+        vorliegt. Das Copyright auf alle durch den Webdesigner erstellten
+        Arbeiten verbleibt beim Webdesigner.
+      </p>
+      <h1 className="mt-4">8. Haftung</h1>
+      <div className="ml-4 lg:ml-12">
+        <p>
+          8.1 &nbsp;&nbsp; Das Risiko der rechtlichen Zulässigkeit der Tätigkeit
+          und Erstellung von Projekten durch den Webdesigner wird von dem Kunden
+          getragen. Der Kunde stellt den Webdesigner von Ansprüchen Dritter
+          frei, wenn dieser auf ausdrücklichen Wunsch des Kunden gehandelt hat,
+          obwohl sie dem Kunden ihre Bedenken im Hinblick auf die Zulässigkeit
+          der Maßnahmen mitgeteilt hat.
+        </p>
+        <p>
+          8.2 &nbsp;&nbsp; Erachtet der Webdesigner für die durchzuführenden
+          Maßnahmen eine wettbewerbsrechtliche Prüfung durch eine besonders
+          sachkundige Person oder Institution für erforderlich, so trägt der
+          Kunde nach Abstimmung die Kosten.
+        </p>
+        <p>
+          8.3 &nbsp;&nbsp; Schadensersatzansprüche gegen den Webdesigner sind
+          ausgeschlossen, sofern sie nicht auf vorsätzlichem oder grob
+          fahrlässigem Verhalten des Designers selbst oder dessen
+          Erfüllungsgehilfen beruhen. Die Verjährungsfrist für die
+          Geltendmachung von Schadensersatz beträgt drei Jahre und beginnt mit
+          dem 5 Zeitpunkt, an dem die Schadensersatzverpflichtung auslösende
+          Handlung begangen worden ist. Sollten die gesetzlichen
+          Verjährungsfristen im Einzelfall für den Webdesigner zu einer kürzeren
+          Verjährung führen, gelten diese.
+        </p>
+        <p>
+          8.4 &nbsp;&nbsp; Für alle weiteren Schadensersatzansprüche gelten die
+          gesetzlichen Bestimmungen des bürgerlichen Gesetzbuches.
+        </p>
+        <p>
+          8.5 &nbsp;&nbsp; Der Höhe nach ist die Haftung des Webdesigners
+          beschränkt auf die bei vergleichbaren Geschäften dieser Art typischen
+          Schäden, die bei Vertragsschluss oder spätestens bei Begehung der
+          Pflichtverletzung vorhersehbar waren.
+        </p>
+        <p>
+          8.6 &nbsp;&nbsp; Die Haftung des Webdesigners für Mangelfolgeschäden
+          aus dem Rechtsgrund der positiven Vertragsverletzung ist
+          ausgeschlossen, wenn und soweit sich die Haftung desselben nicht aus
+          einer Verletzung der für die Erfüllung des Vertragszweckes
+          wesentlichen Pflichten ergibt.
+        </p>
+      </div>
+      <h1 className="mt-4">9. Maßgebliches Recht und Gerichtsstand</h1>
+      <div className="ml-4 lg:ml-12">
+        <p>
+          9.1 &nbsp;&nbsp; Für die Geschäftsverbindung zwischen dem Kunden und
+          dem Webdesigner gilt deutsches Recht.
+        </p>
+        <p>
+          9.2 &nbsp;&nbsp; Die Gerichtsstandvereinbarung gilt für Inlandskunden
+          und Auslandskunden gleichermaßen.
+        </p>
+        <p>
+          9.3 &nbsp;&nbsp; Erfüllungsort und Gerichtsstand für alle Leistungen
+          und Auseinandersetzungen ist ausschließlich der Sitz des Webdesigners
+        </p>
+      </div>
+      <h1 className="mt-4">10. Sonstige Bestimmungen</h1>
+      <p className="ml-4 lg:ml-12">
+        Mündliche Nebenabreden zu diesem Vertrag bestehen nicht.
+      </p>
+    </div>
+  );
 }
