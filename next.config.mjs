41b1f06f126abe31c728e9701bc78c0564b08cde
Marian Noekel <marian@MacCarlo.local>
2024-07-28 16:13:34 +0200

    disable server-based image optimization.

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/namaste-websites", // basePath sets the path for github pages
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-  };
+  basePath: "/namaste-websites", // basePath sets the path for github pages
+  output: "export", // <=== enables static exports
+  reactStrictMode: true,
+  /**
+   * Disable server-based image optimization. Next.js does not support
+   * dynamic features with static exports.
+   *
+   * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+   */
+  images: {
+    unoptimized: true,
+  },
+};
 
 export default nextConfig;
