b6061db9bbbe56060d07adc5ab13c87c8e880dc5
Marian Noekel <marian@MacCarlo.fritz.box>
2025-01-12 17:16:48 +0100

    add margin at contact view

--- src/app/Kontakt/page.tsx
+++ src/app/Kontakt/page.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import Contact from "../components/Contact";
 export default function Kontakt() {
   return (
-    <div className="mx-4">
+    <div className="mx-4 my-8 lg:my-16">
       <div className="lg:p-4 max-w-3xl md:mx-auto">
         <h1 className="md:text-3xl md:font-thin mb-4"> Ich freue mich darauf, von Dir zu hören!</h1>
         <p className="md:text-xl">
